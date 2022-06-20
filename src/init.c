0d47079ae958eaaad8d321b2dbcb22e1571fe35f
John Decorte <jdecorte@fu-r5-p7.s19.be>
2022-06-20 20:24:07 +0200

    refactor(assets)!: Restructure game assets and update animation frames

--- src/init.c
+++ src/init.c
@@ -24,50 +24,38 @@ void		set_plane(t_data *ptr, char flag)
 	}
 }
 
-void	init_texture(t_data *ptr, int flag, char *path)
+void			init_texture(t_data *ptr, int flag, char *path)
 {
-	ptr->img[flag]->p_img = mlx_xpm_file_to_image(ptr->mlx,
-			path, &ptr->img[flag]->width, &ptr->img[flag]->height);
-	if (!ptr->img[flag]->p_img)
+	if (!(ptr->img[flag]->p_img = mlx_xpm_file_to_image(ptr->mlx,
+	path, &ptr->img[flag]->width, &ptr->img[flag]->height)))
 		exit(write_error("Error\nMap load\n"));
 	ptr->img[flag]->p_img = mlx_get_data_addr(ptr->img[flag]->p_img,
-			&ptr->img[flag]->bt, &ptr->img[flag]->s_line,
-			&ptr->img[flag]->endian);
+	&ptr->img[flag]->bt, &ptr->img[flag]->s_line, &ptr->img[flag]->endian);
 }
 
-void	init_data(t_data *data)
+void    init_data(t_data *data)
 {
-	int	i;
+    data->mlx = mlx_init();
+    data->mlx_win = mlx_new_window(data->mlx, screenWidth, screenHeight, "cub3d");
 
-	data->mlx = mlx_init();
-	data->mlx_win = mlx_new_window(data->mlx,
-			screenWidth, screenHeight, "cub3d");
-	data->pl = malloc(sizeof(t_player));
-	data->dda = malloc(sizeof(t_dda));
-	data->map = malloc(sizeof(t_map));
-	data->t = malloc(sizeof(t_time));
-	if (!data->pl || !data->dda || !data->map || !data->t)
-		return ;
-	data->t->time = 0;
-	data->t->oldtime = 0;
-	i = 0;
-	data->img = malloc(sizeof(t_img *) * 8);
-	if (!data->img)
-		return ;
-	while (i <= 7)
+    data->pl = malloc(sizeof(t_player));
+    data->dda = malloc(sizeof(t_dda));
+    data->map = malloc(sizeof(t_map));
+    data->t = malloc(sizeof(t_time));
+
+    int i = 0;
+    if (!(data->img = malloc(sizeof(t_img *) * 13)))
+        return ;
+	while (i <= 12)
 	{
-		data->img[i] = malloc(sizeof(t_img));
-		if (!data->img[i])
+		if (!(data->img[i] = malloc(sizeof(t_img))))
 			return ;
 		i++;
 	}
 	data->sp = malloc(sizeof(t_sprite));
-	if (!data->sp)
-		return ;
 	data->sp->buffer = malloc(sizeof(double) * (screenWidth + 1));
-	if (!data->sp)
-		return ;
-	data->pl->totalrots = 0;
+
+    data->pl->totalrots = 0;
 }
 
 void	pl_init(t_data *data)
